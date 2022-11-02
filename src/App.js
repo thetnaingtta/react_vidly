925613af67221535bef746bead81313ac7e2d769
Thet Naing <thetnaing@mirandah.com>
2022-11-02 10:34:18 +0800

    adding navBar and Form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import "./App.css";
-import Movies from "./components/movies";
 import { Redirect, Route, Switch } from "react-router-dom";
 import NotFound from "./components/notFound";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
+import Movies from "./components/movies";
+import NavBar from "./components/navBar";
+import "./App.css";
+import React from "react";
+import MovieForm from "./components/movieForm";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies} />
-        <Route path="/rentals" component={Rentals} />
-        <Route path="/customers" component={Customers} />
-        <Route path="/not-found" component={NotFound} />
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
